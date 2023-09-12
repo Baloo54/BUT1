3555fe5c427a2889bab5c08459eabbdef4299ae6
Baloo54 <gabriel.comte2006@gmail.com>
2023-09-12 15:03:23 +0200

    exo13fini

--- archi/progC/exo13.c
+++ archi/progC/exo13.c
@@ -2,28 +2,23 @@
 #include <string.h>
 int main(void)
 {
-    char tab[1000],tab2[10][10], tab3[1000];
-    int i, j, h;
+    char tab[1000], tab2[1000];
+    int i, h, j = 0;
     printf("écrivez un texte:\n");
     fgets(tab, sizeof(tab), stdin); 
-    j = 0; h = 0;
-    for(i = 0; i < strlen(tab); i++)
+    tab[strlen(tab)] = ' ';
+    i = strlen(tab) - 3;
+    for(i; i >= 0; i--)
     {
-        if (tab[i] == ' ')j++, h = 0;
-        else
+        if((tab[i] == ' ')| (i == 0))
         {
-            tab2[j][h++] = tab[i];
+            h = (i == 0) ? i : i + 1;
+            while (tab[h] != ' ')
+            {
+                tab2[j++] = tab[h++];
+            }      
+        tab2[j++] = ' ';
         }
     }
-    j = 0; h = 0;
-    for(i = (sizeof(tab2)/sizeof(tab2[0])); i >= 0; i--)
-    {
-        for(j = 0; j < strlen(tab2[i]); j++)
-        {
-            if((tab2[i][j] < 'A') | (tab2[i][j] > '~') | (tab2[i][j] == ' ') | (tab2[i][j] == '`'))continue;
-            tab3[h++] = tab2[i][j]; 
-        }
-        tab3[h++] = ' ';
-    }
-    printf("%s\n", tab3);
+    printf("%s\n", tab2);
 }
--- archi/progC/exo8.c
+++ archi/progC/exo8.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 int main(void){
-  int Majuscule(void){
-    char lettre;
-    while (printf("Donnez une lettre minuscule\n"),lettre = getchar(),(lettre<'`')||(lettre>'{')); 
-    printf("%c\n", lettre -32);
-  }
-  Majuscule();
+  char lettre;
+  while (printf("Donnez une lettre minuscule\n"),lettre = getchar(),(lettre<'`')||(lettre>'{')); 
+  printf("%c\n", lettre -32);
 }
